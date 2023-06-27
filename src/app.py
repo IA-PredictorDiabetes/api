ccd1773aaec8adca218c56ca06e3e42457c2fa1a
rolitoxdd <rolo.alv97@gmail.com>
2023-06-27 00:19:44 -0400

    fix

--- src/app.py
+++ src/app.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 print('Cargando modelo...')
 # Cargar las credenciales desde el archivo JSON
 # credentials_path = './secrets/predictordiabetes-eac5e9c634e7.json'  # Reemplaza con la ruta a tu archivo de credenciales JSON
-credentials_path = environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+# credentials_path = environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+credentials_path='/home/rolito/api/src/credentials.json'
 creds = service_account.Credentials.from_service_account_file(credentials_path)
 
 # Conectarse a Google Cloud Storage utilizando las credenciales
@@ -19,13 +20,13 @@ storage_client = storage.Client(credentials=creds)
 bucket_name = 'predictor_diabetes'  # Reemplaza con el nombre de tu bucket en Google Cloud Storage
 file_name = 'modelo_diabetes.pkl'  # Reemplaza con el nombre de tu archivo .pkl
 bucket = storage_client.get_bucket(bucket_name)
-blob = bucket.blob(file_name)
+# blob = bucket.blob(file_name)
 
-# Descargar el archivo .pkl como bytes
-pkl_data = blob.download_as_bytes()
+# # Descargar el archivo .pkl como bytes
+# pkl_data = blob.download_as_bytes()
 
 # Cargar el archivo .pkl
-modelo = pickle.loads(pkl_data)
+modelo = pickle.loads(bucket.blob(file_name).download_as_bytes())
 print('Modelo cargado')
 
 @app.route('/consulta', methods=['POST'])
@@ -34,7 +35,9 @@ def consulta():
     datos = request.get_json()
 
     # Realizar la consulta al modelo
+    print('datos',datos)
     resultado = modelo.predict(datos)
+    print(resultado, 'resultado')
 
     # Devolver la respuesta como JSON
     return jsonify({'resultado': resultado.tolist()})
